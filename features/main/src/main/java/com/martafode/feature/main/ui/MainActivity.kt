e42beebad18e34ac091572e75823f51855bfd6b2
Marta Foderaro <matij.eng@gmail.com>
2021-01-20 17:50:54 +0000

    Add main feature module

---
+++ features/main/src/main/java/com/martafode/feature/main/ui/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.martafode.feature.main.ui
+
+import com.martafode.feature.main.R
+import com.martafode.lib.ui.mvrx.BaseActivity
+
+class MainActivity: BaseActivity(R.layout.activity_main) {
+
+}
