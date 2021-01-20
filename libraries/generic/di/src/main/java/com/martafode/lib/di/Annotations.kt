a7d648bdaf91a731bdb7d0f41bbdd6568805daff
Marta Foderaro <matij.eng@gmail.com>
2021-01-20 16:18:16 +0000

    Add di module to hold dependency injection annotations and more eventual generic di-related objects

---
+++ libraries/generic/di/src/main/java/com/martafode/lib/di/Annotations.kt
@@ -0,0 +1,19 @@
+// ktlint-disable filename
+@file:Suppress("unused")
+
+package com.martafode.lib.di
+
+import javax.inject.Scope
+
+/**
+ * Annotation for [Application] scoped objects.
+ *
+ * In Dagger, an unscoped component cannot depend on a scoped component. As
+ * [AppComponent] is a scoped component (`@Singleton`, we create a custom scope
+ * to be used by all activity components. Additionally, a component with
+ * a specific scope cannot have a sub component with the same scope.
+ */
+@MustBeDocumented
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationScoped
