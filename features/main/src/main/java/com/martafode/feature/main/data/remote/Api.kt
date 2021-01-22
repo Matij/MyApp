936bbc0975d9280ec7054efafc8216e8b7fbd74c
Marta Foderaro <matij.eng@gmail.com>
2021-01-22 11:46:14 +0000

    Remove unusable methods (because of api ACCESS KEY limitations)

--- features/main/src/main/java/com/martafode/feature/main/data/remote/Api.kt
+++ features/main/src/main/java/com/martafode/feature/main/data/remote/Api.kt
@@ -25,37 +25,6 @@ interface Api {
     ): ApiExchangeRateData
 
     @JsonClass(generateAdapter = true)
-    data class ApiQuery(
-        @Json(name = "from") val from: String,
-        @Json(name = "to") val to: String,
-        @Json(name = "amount") val amount: String,
-    )
-
-    @JsonClass(generateAdapter = true)
-    data class ApiInfo(
-        @Json(name = "timestamp") val from: Long,
-        @Json(name = "quote") val to: Double,
-    )
-
-    @JsonClass(generateAdapter = true)
-    data class ApiConversion(
-        @Json(name = "source") val source: String,
-        @Json(name = "terms") val terms: String,
-        @Json(name = "privacy") val privacy: String,
-        @Json(name = "query") val query: ApiQuery,
-        @Json(name = "info") val info: ApiInfo,
-        @Json(name = "result") val result: String,
-    )
-
-    @GET("convert")
-    suspend fun fetchAmountConversion(
-        @Query("access_key") accessKey: String,
-        @Query("from") from: String,
-        @Query("to") to: String,
-        @Query("amount") amount: String,
-    ): ApiConversion
-
-    @JsonClass(generateAdapter = true)
     data class ApiCurrencyData(
         @Json(name = "success") val success: Boolean,
         @Json(name = "timestamp") val timestamp: String?,
